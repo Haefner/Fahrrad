ad02ba426ae593d3f315268ce7b5a3e24b088cc6
haefner <lisa-maria.haefner@hs-bochum.de>
2019-04-08 14:01:10 +0200

    Tabelle mit Inhalt gefüllt

--- src/Model/Fahrrad.java
+++ src/Model/Fahrrad.java
@@ -3,32 +3,57 @@ package Model;
 public class Fahrrad {
 	private String name;
 	private String artikelnummer;
+	private String typ;
 	private String farbe;
-	public Fahrrad(String name, String artikelnummer, String farbe, int zoll) {
+	private int zoll;
+
+	public Fahrrad(String name, String artikelnummer, String typ, String farbe, int zoll) {
 		super();
 		this.name = name;
 		this.artikelnummer = artikelnummer;
 		this.farbe = farbe;
 		this.zoll = zoll;
+		this.typ = typ;
 	}
+
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public String getFarbe() {
+		return farbe;
+	}
+
+	public void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getArtikelnummer() {
 		return artikelnummer;
 	}
+
 	public void setArtikelnummer(String artikelnummer) {
 		this.artikelnummer = artikelnummer;
 	}
+
 	public int getZoll() {
 		return zoll;
 	}
+
 	public void setZoll(int zoll) {
 		this.zoll = zoll;
 	}
-	private int zoll;
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -2,15 +2,18 @@ package application;
 
 import Model.Fahrrad;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -21,13 +24,15 @@ public class Main extends Application {
 		TableView<Fahrrad> table = new TableView<Fahrrad>();
 
 		TableColumn<Fahrrad, String> bezeichnung = new TableColumn<Fahrrad, String>("Bezeichung");
-		
+
 		TableColumn<Fahrrad, String> name = new TableColumn<Fahrrad, String>("Name");
 
 		TableColumn<Fahrrad, String> artikelnummer//
 				= new TableColumn<Fahrrad, String>("Artikelnummer");
-		
-		bezeichnung.getColumns().addAll(name,artikelnummer);
+		TableColumn<Fahrrad, String> typ//
+				= new TableColumn<Fahrrad, String>("Typ");
+
+		bezeichnung.getColumns().addAll(name, artikelnummer);
 
 		TableColumn<Fahrrad, String> farbe//
 				= new TableColumn<Fahrrad, String>("Farbe");
@@ -35,7 +40,17 @@ public class Main extends Application {
 		TableColumn<Fahrrad, Integer> zoll //
 				= new TableColumn<Fahrrad, Integer>("Zoll");
 
-		table.getColumns().addAll(bezeichnung, farbe, zoll);
+		table.getColumns().addAll(bezeichnung, typ, farbe, zoll);
+
+		// Definiere, welcher Wert des Objektes Fahrrad zu welcher Tabellenzeile gehört
+		name.setCellValueFactory(new PropertyValueFactory<>("name"));
+		artikelnummer.setCellValueFactory(new PropertyValueFactory<>("artikelnummer"));
+		typ.setCellValueFactory(new PropertyValueFactory<>("typ"));
+		farbe.setCellValueFactory(new PropertyValueFactory<>("farbe"));
+		zoll.setCellValueFactory(new PropertyValueFactory<>("zoll"));
+
+		// Füge einträge hinzu
+		table.setItems(getRadList());
 
 		StackPane root = new StackPane();
 		root.setPadding(new Insets(3));
@@ -46,5 +61,15 @@ public class Main extends Application {
 		Scene scene = new Scene(root, 450, 300);
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	private ObservableList<Fahrrad> getRadList() {
+
+		Fahrrad rad1 = new Fahrrad("Green Bike", "1234", "Cityrad", "Grün", 28);
+		Fahrrad rad2 = new Fahrrad("Blitz", "1245", "Rennrad", "Gelb", 28);
+		Fahrrad rad3 = new Fahrrad("Pink Bike", "12456", "Mountainbike", "Pink", 28);
+
+		ObservableList<Fahrrad> list = FXCollections.observableArrayList(rad1, rad2, rad3);
+		return list;
 	}
 }
