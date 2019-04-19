5d45a9918f893e851f6d613f4bd51f36f5e75fcf
haefner <lisa-maria.haefner@hs-bochum.de>
2019-04-19 18:36:07 +0200

    Main aufgeräumt

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import Model.Fahrrad;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
@@ -12,10 +13,25 @@ import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	private TableView<Fahrrad> fahrradTablle;
+
+	private TableColumn<Fahrrad, String> typ;
+
+	private TableColumn<Fahrrad, String> farbe;
+
+	private TableColumn<Fahrrad, Integer> zoll;
+
+	private TableColumn<Fahrrad, String> name;
+
+	private TableColumn<Fahrrad, String> bezeichnung;
+
+	private TableColumn<Fahrrad, String> artikelnummer;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -24,42 +40,61 @@ public class Main extends Application {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage stage) {
+		stage.setTitle("TableView (Fahrrad)");
 
-		TableView<Fahrrad> table = new TableView<Fahrrad>();
+		//Tabelle anlegen
+		generriereTabellenUndUeberschriften();
+		ordneTabellenzeilenZuVariablenDesObjektes();
+		aenderungDerOberflaeche();
 
-		TableColumn<Fahrrad, String> typ = new TableColumn<Fahrrad, String>("Typ");
+		AnchorPane root = new AnchorPane();
+		root.setPadding(new Insets(3));
+		
+		// Fuege Eintraege hinzu
+		fahrradTablle.setItems(getRadList());
+		root.getChildren().add(fahrradTablle);
+		
+		//Fügt dem Fenster die Zene mit der Tabelle hinzuhinzu
+		Scene scene = new Scene(root, 450, 300);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-		TableColumn<Fahrrad, String> farbe = new TableColumn<Fahrrad, String>("Farbe");
-
-		TableColumn<Fahrrad, Integer> zoll = new TableColumn<Fahrrad, Integer>("Zoll");
-
-		TableColumn<Fahrrad, String> bezeichnung = new TableColumn<Fahrrad, String>("Bezeichung");
-
-		TableColumn<Fahrrad, String> name = new TableColumn<Fahrrad, String>("Name");
-
-		TableColumn<Fahrrad, String> artikelnummer = new TableColumn<Fahrrad, String>("Artikelnummer");
-
+	/**
+	 * Tabellenzeilen auf der Oberfläche anlegen
+	 */
+	private void generriereTabellenUndUeberschriften() {
+		fahrradTablle = new TableView<Fahrrad>();
+		typ = new TableColumn<Fahrrad, String>("Typ");
+		farbe = new TableColumn<Fahrrad, String>("Farbe");
+		zoll = new TableColumn<Fahrrad, Integer>("Zoll");
+		bezeichnung = new TableColumn<Fahrrad, String>("Bezeichung");
+		name = new TableColumn<Fahrrad, String>("Name");
+		artikelnummer = new TableColumn<Fahrrad, String>("Artikelnummer");
 		bezeichnung.getColumns().addAll(name, artikelnummer);
+		fahrradTablle.getColumns().addAll(bezeichnung, typ, farbe, zoll);
+	}
 
-		table.getColumns().addAll(bezeichnung, typ, farbe, zoll);
-
-		// Definiere, welcher Wert des Objektes Fahrrad zu welcher Tabellenzeile gehört
+	/**
+	 * Definiere, welcher Wert des Objektes Fahrrad zu welcher Tabellenzeile gehört
+	 */
+	private void ordneTabellenzeilenZuVariablenDesObjektes() {
 		name.setCellValueFactory(new PropertyValueFactory<Fahrrad, String>("name"));
-
 		artikelnummer.setCellValueFactory(new PropertyValueFactory<>("artikelnummer"));
 		typ.setCellValueFactory(new PropertyValueFactory<>("typ"));
 		farbe.setCellValueFactory(new PropertyValueFactory<>("farbe"));
 		zoll.setCellValueFactory(new PropertyValueFactory<>("zoll"));
+	}
 
-		// Füge einträge hinzu
-		table.setItems(getRadList());
-
-		// Ändern der Tabelle
-
-		table.setEditable(true);
-
+	/**
+	 * Ermöglicht, dass der Name des Fahrrades auf der Oberfläche geändert werden kann
+	 */
+	private void aenderungDerOberflaeche() {
+		//Allgemein das ändern in der Tabelle ermöglichen
+		fahrradTablle.setEditable(true);
+		//Definiert, das die Tabellenzelle änderbar ist
 		name.setCellFactory(TextFieldTableCell.<Fahrrad>forTableColumn());
-
+		//Sorgt dafür, dass die Änderungen an der Oberfläche registriert werden
 		name.setOnEditCommit((CellEditEvent<Fahrrad, String> event) -> {
 			String newName = event.getNewValue();
 			TablePosition<Fahrrad, String> pos = event.getTablePosition();
@@ -68,16 +103,6 @@ public class Main extends Application {
 			r.setName(newName);
 
 		});
-
-		StackPane root = new StackPane();
-		root.setPadding(new Insets(3));
-		root.getChildren().add(table);
-
-		stage.setTitle("TableView (Fahrrad)");
-
-		Scene scene = new Scene(root, 450, 300);
-		stage.setScene(scene);
-		stage.show();
 	}
 
 	private ObservableList<Fahrrad> getRadList() {
