a8db206833e27a06a26d4c5e8e0d223c474f6190
haefner <lisa-maria.haefner@hs-bochum.de>
2019-04-19 18:52:28 +0200

    MainMitFXML aufgeräumt

--- src/viewFXML/MainMitFXML.java
+++ src/viewFXML/MainMitFXML.java
@@ -2,75 +2,39 @@ package viewFXML;
 
 import java.io.IOException;
 
-import Model.Fahrrad;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainMitFXML extends Application {
-	
-	private Stage primaryStage;
-    private AnchorPane rootLayout;
-    
 
-	public static void main(String[] args)  {
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage stage) {
-		 this.primaryStage = stage;
-		 this.primaryStage.setTitle("TableView (Fahrrad)");
-		initRootLayout();
+		this.primaryStage = stage;
+		this.primaryStage.setTitle("TableView (Fahrrad)");
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainMitFXML.class.getResource("tabelle.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	/**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainMitFXML.class.getResource("tabelle.fxml"));
-            rootLayout = (AnchorPane) loader.load();
-            
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showFahrradOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainMitFXML.class.getResource("tabelle.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
-
-            // Give the controller access to the main app.
-            FahradOverviewController controller = loader.getController();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
 }
-
