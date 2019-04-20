53933bbae7518fe88bc17e6bdb6807235551c81c
haefner <lisa-maria.haefner@hs-bochum.de>
2019-04-20 10:30:25 +0200

    Enum Typ toString hinzugefügt

--- src/Model/Typ.java
+++ src/Model/Typ.java
@@ -1,7 +1,20 @@
 package Model;
 
 public enum Typ {
-	
+
 	MOUNTAINBIKE, CITYRAD, RENNRAD;
+
+	public String toString() {
+		switch (this) {
+		case MOUNTAINBIKE:
+			return "Mountainbike";
+		case CITYRAD:
+			return "Cityrad";
+		case RENNRAD:
+			return "Rennrad";
+		default:
+			return "";
+		}
+	}
 
 }
