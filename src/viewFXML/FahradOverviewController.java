223173848e98ca6f6950d271321a35e273aca936
haefner <lisa-maria.haefner@hs-bochum.de>
2019-04-22 13:08:53 +0200

    Neue Fahrräder können angelegt werden

--- src/viewFXML/FahradOverviewController.java
+++ src/viewFXML/FahradOverviewController.java
@@ -12,19 +12,21 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
 
-public class FahradOverviewController{
+public class FahradOverviewController {
 	@FXML
 	private TableView<Fahrrad> fahrradTablle;
 	@FXML
@@ -37,6 +39,18 @@ public class FahradOverviewController{
 	private TableColumn<Fahrrad, String> name;
 	@FXML
 	private TableColumn<Fahrrad, String> artikelnummer;
+
+	// Reiter hinzufuegen
+	@FXML
+	private TextField nameNeu;
+	@FXML
+	private TextField artikelnummerNeu;
+	@FXML
+	private ComboBox<Typ> typNeu;
+	@FXML
+	private TextField farbeNeu;
+	@FXML
+	private TextField zollNeu;
 
 	// Reference to the main application.
 	private MainMitFXML mainApp;
@@ -53,8 +67,10 @@ public class FahradOverviewController{
 		fahrradTablle.setItems(getRadList());
 		aenderungDerOberflaeche();
 		contextMenuDelete();
+		initalizeComboBoxTypNeu();
 
 	}
+
 
 	/**
 	 * Definiere, welcher Wert des Objektes Fahrrad zu welcher Tabellenzeile gehört
@@ -66,58 +82,55 @@ public class FahradOverviewController{
 		farbe.setCellValueFactory(new PropertyValueFactory<>("farbe"));
 		zoll.setCellValueFactory(new PropertyValueFactory<>("zoll"));
 	}
-	
+
 	/**
-	 * Ermöglicht, dass der Name des Fahrrades auf der Oberfläche geändert werden kann
+	 * Ermöglicht, dass der Name des Fahrrades auf der Oberfläche geändert werden
+	 * kann
 	 */
 	private void aenderungDerOberflaeche() {
-		//Allgemein das ändern in der Tabelle ermöglichen
+		// Allgemein das ändern in der Tabelle ermöglichen
 		fahrradTablle.setEditable(true);
-		//Fügt der Tabellenzeile einen Editor zur Textänderung hinzu
+		// Fügt der Tabellenzeile einen Editor zur Textänderung hinzu
 		name.setCellFactory(TextFieldTableCell.<Fahrrad>forTableColumn());
-		//Sorgt dafür, dass die Änderungen an der Oberfläche registriert werden
+		// Sorgt dafür, dass die Änderungen an der Oberfläche registriert werden
 		name.setOnEditCommit((CellEditEvent<Fahrrad, String> event) -> {
 			String newName = event.getNewValue();
-			int row  = event.getTablePosition().getRow();
+			int row = event.getTablePosition().getRow();
 			Fahrrad r = event.getTableView().getItems().get(row);
 			r.setName(newName);
 		});
-		
+
 		artikelnummer.setCellFactory(TextFieldTableCell.<Fahrrad>forTableColumn());
 	}
-	
-	private void contextMenuDelete()
-	{
-		 fahrradTablle.setRowFactory(new Callback<TableView<Fahrrad>, TableRow<Fahrrad>>() {  
-	            @Override  
-	            public TableRow<Fahrrad> call(TableView<Fahrrad> tableView) {  
-	                final TableRow<Fahrrad> row = new TableRow<>();  
-	                final ContextMenu contextMenu = new ContextMenu();  
-	                final MenuItem removeMenuItem = new MenuItem("Löschen");  
-	                removeMenuItem.setOnAction(new EventHandler<ActionEvent>() {  
-	                    @Override  
-	                    public void handle(ActionEvent event) {  
-	                    	fahrradTablle.getItems().remove(row.getItem());  
-	                    }  
-	                });  
-	                contextMenu.getItems().add(removeMenuItem);  
-	               // Set context menu on row, but use a binding to make it only show for non-empty rows:  
-	                row.contextMenuProperty().bind(  
-	                        Bindings.when(row.emptyProperty())  
-	                        .then((ContextMenu)null)  
-	                        .otherwise(contextMenu)  
-	                );  
-	                return row ;  
-	            }  
-	        });  
+
+	private void contextMenuDelete() {
+		fahrradTablle.setRowFactory(new Callback<TableView<Fahrrad>, TableRow<Fahrrad>>() {
+			@Override
+			public TableRow<Fahrrad> call(TableView<Fahrrad> tableView) {
+				final TableRow<Fahrrad> row = new TableRow<>();
+				final ContextMenu contextMenu = new ContextMenu();
+				final MenuItem removeMenuItem = new MenuItem("Löschen");
+				removeMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						fahrradTablle.getItems().remove(row.getItem());
+					}
+				});
+				contextMenu.getItems().add(removeMenuItem);
+				// Set context menu on row, but use a binding to make it only show for non-empty
+				// rows:
+				row.contextMenuProperty()
+						.bind(Bindings.when(row.emptyProperty()).then((ContextMenu) null).otherwise(contextMenu));
+				return row;
+			}
+		});
 	}
-	
+
 	@FXML
-	public void aenderArtikelnummer(CellEditEvent<Fahrrad, String> event)
-	{
+	public void aenderArtikelnummer(CellEditEvent<Fahrrad, String> event) {
 		String newName = event.getNewValue();
-		//int row  = event.getTablePosition().getRow();
-		int row =fahrradTablle.getSelectionModel().getSelectedIndex();
+		// int row = event.getTablePosition().getRow();
+		int row = fahrradTablle.getSelectionModel().getSelectedIndex();
 		Fahrrad r = event.getTableView().getItems().get(row);
 		r.setArtikelnummer(newName);
 	}
@@ -129,6 +142,21 @@ public class FahradOverviewController{
 
 		ObservableList<Fahrrad> list = FXCollections.observableArrayList(rad1, rad2, rad3);
 		return list;
+	}
+
+	@FXML
+	public void hinzufuegen() {
+		Fahrrad rad1 = new Fahrrad(nameNeu.getText(), artikelnummerNeu.getText(), typNeu.getValue(), farbeNeu.getText(),
+				Integer.parseInt(zollNeu.getText()));
+		nameNeu.clear();
+		artikelnummerNeu.clear();
+		zollNeu.clear();
+		fahrradTablle.getItems().add(rad1);
+	}
+	
+	private void initalizeComboBoxTypNeu() {
+		typNeu.getItems().addAll(Typ.values());
+		
 	}
 
 }
